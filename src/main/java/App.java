e50930d983df2712d2a55b45964e2a3234bc4be9
RML-Cambo <ltomusic69@gmail.com>
2025-04-18 19:32:18 +0700

    UI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,16 @@
-import controller.LanternaStockController;
 import controller.StockController;
 
 public class App {
     public static void main(String[] args) {
-        LanternaStockController controller = new LanternaStockController();
-        controller.start();
+        new StockController().start();
     }
 }
+
+// for use Lanterna ui, but it have some error that I don't have to complete yet! :
+
+//public class App {
+//    public static void main(String[] args) {
+//        LanternaStockController controller = new LanternaStockController();
+//        controller.start();
+//    }
+//}
